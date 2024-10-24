9a2d5a52e7c421dbc2d09470e79871fa9afd06d8
RCMaia <rfpcmaia@hotmail.com>
2024-10-24 14:30:27 -0400

    bla

--- Sprint-2/__tests__/simple.test.js
+++ Sprint-2/__tests__/simple.test.js
@@ -4,4 +4,9 @@ test("1 + 1 = 2", () => {
   expect(1 + 1).toBe(2);
 });
 
-test(db, () => {});
+const sql =
+  "INSERT INTO teacher (ID, Username, Password) Values ('1', 'Z', '123')";
+
+test(sql, () => {
+  db.query(sql);
+});
